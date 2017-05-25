7bb9680b5226c4625ecd98c41c9585a4b53f5d99
Nina Chikanov <nchikanov@gmail.com>
2017-05-24 19:59:52 -0700

    Finish two categories (color & weather), transition to closetView screen

--- My_Closet/ClosetViewController.swift
+++ My_Closet/ClosetViewController.swift
@@ -11,18 +11,66 @@ import UIKit
 
 class ClosetViewController: UIViewController {
     
-    var test = "hi"
+    @IBOutlet var shoeLabel: UILabel!
+    @IBOutlet var pantLabel: UILabel!
+    @IBOutlet var shirtLabel: UILabel!
+    @IBOutlet var hatLabel: UILabel!
     
-    @IBOutlet var label: UILabel!
+    @IBOutlet var shuffleButton: UIButton!
+    
+    var testArray: [String] = [String]()
+    var summerHatArray: [String] = [String]()
+    var summerShirtArray: [String] = [String]()
+    var summerPantArray: [String] = [String]()
+    var summerShoeArray: [String] = [String]()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        summerHatArray = ["Fedora", "Straw Hat", "Baseball Cap", "Visor", "Helmet", "Bucket Hat"]
+        summerShirtArray = ["Tank", "Crop Top", "T-Shirt", "BU!!!!", "Bikini Top", "Jersey"]
+        summerPantArray = ["Jeans", "Khakis", "Skirt", "Shorts", "Daisy Dukes", "Bikini Bottoms"]
+        summerShoeArray = ["Flip Flops", "Sandals", "Tevas", "Vans", "Converse", "Barefoot", "Sperry's"]
+        
+        let random = Int (arc4random_uniform(_:UInt32(summerHatArray.count)))
+        self.hatLabel.text = summerHatArray[random]
+        
+        let random1 = Int (arc4random_uniform(_:UInt32(summerShirtArray.count)))
+        self.shirtLabel.text = summerShirtArray[random1]
+        
+        let random2 = Int (arc4random_uniform(_:UInt32(summerPantArray.count)))
+        self.pantLabel.text = summerPantArray[random2]
+        
+        let random3 = Int (arc4random_uniform(_:UInt32(summerShoeArray.count)))
+        self.shoeLabel.text = summerShoeArray[random3]
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        NSLog(test)
+        
+        for i in testArray{
+            NSLog(i) // Each item here corresponds to a category (Sunny, Pastel) 
+            // Can also access directly through testArray 
+        }
     }
+    
+    @IBAction func randomizeAllFields(_ sender: Any) {
+        let random = Int (arc4random_uniform(_:UInt32(summerHatArray.count)))
+        self.hatLabel.text = summerHatArray[random]
+        
+        let random1 = Int (arc4random_uniform(_:UInt32(summerShirtArray.count)))
+        self.shirtLabel.text = summerShirtArray[random1]
+        
+        let random2 = Int (arc4random_uniform(_:UInt32(summerPantArray.count)))
+        self.pantLabel.text = summerPantArray[random2]
+        
+        let random3 = Int (arc4random_uniform(_:UInt32(summerShoeArray.count)))
+        self.shoeLabel.text = summerShoeArray[random3]
+    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- My_Closet/ViewController.swift
+++ My_Closet/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBOutlet var weatherPicker: UIPickerView!
 
     @IBOutlet var colorPickerButton: UIButton!
+    @IBOutlet var colorPicker: UIPickerView!
     @IBOutlet var weatherPickerButton: UIButton!
     
     var wPickerData: [String] = [String]()
@@ -20,20 +21,26 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     @IBOutlet var greyView: UIView!
     
+    
+    // Send Data from first screen to second
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ShowClosetSegue" {
             if let destination = segue.destination as? ClosetViewController {
-                destination.test = "test"
+                var testOutput: [String] = [String]()
+                
+                if let wTitle = weatherPickerButton.currentTitle {
+                    testOutput.append(wTitle)
+                }
+                
+                if let csTitle = colorPickerButton.currentTitle {
+                    testOutput.append(csTitle)
+                }
+                
+                destination.testArray = testOutput
             }
         }
     }
     
-    
-    /* TO DO:
-     1. Figure out why picker is being loaded for all buttons.
-     2. See if you can pass different data to the same picker or if we have to make a new picker. 
-     3. Where is the data actually being loaded into the picker? It defaults to the weather picker data each time. I'm trying to load in the color scheme data. 
-    */
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,17 +48,20 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         self.weatherPicker.delegate = self
         self.weatherPicker.dataSource = self
         
+        self.colorPicker.dataSource = self
+        self.colorPicker.delegate = self
+        
         csPickerData = ["Neutral", "Bright", "Pastel", "Solid"]
         wPickerData = ["Sunny", "Cloudy", "Windy", "Raining", "Really Long Option"]
         
         
         weatherPicker.isHidden = true
+        colorPicker.isHidden = true
         greyView.isHidden = true
     
     
     weatherPickerButton.setTitle("Choose...", for: .normal)
-        colorPickerButton.setTitle("Choose...", for: .normal)
-    
+    colorPickerButton.setTitle("Choose...", for: .normal)
         
         
     }
@@ -69,21 +79,57 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     // The numbers of rows of data
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        
-        return wPickerData.count
-        
+        if (pickerView.tag == 0) {// weather
+            return wPickerData.count
+        }
+        else if (pickerView.tag == 1) {//color
+            return csPickerData.count
+        }
+        else{
+            return 0
+        }
     }
     
     
     // The data to return for the row and component(column) that's being passed in
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return wPickerData[row]
+        
+        if (pickerView.tag == 0) {// weather
+            return wPickerData[row]
+        }
+        else if (pickerView.tag == 1) {//color
+            return csPickerData[row]
+        }
+        else{
+            return "Choose..."
+        }
+
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        weatherPickerButton.setTitle(wPickerData[row], for: .normal)
-        weatherPicker.isHidden = true
+        
+        if (pickerView.tag == 0){
+            weatherPickerButton.setTitle(wPickerData[row], for: .normal)
+            weatherPicker.isHidden = true
+        }
+        else if (pickerView.tag == 1) {
+            colorPickerButton.setTitle(csPickerData[row], for: .normal)
+            colorPicker.isHidden = true
+        }
+        else {
+            
+        }
+    
         greyView.isHidden = true
+    }
+    
+    @IBAction func showColorSelection(_ sender: Any) {
+        if (colorPicker.isHidden) {
+            colorPicker.isHidden = false
+            greyView.isHidden = false
+        } else {
+            colorPicker.isHidden = true
+        }
     }
     
     @IBAction func showWeatherSelection(_ sender: Any) {
